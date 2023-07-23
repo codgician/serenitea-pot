670b35bc60e37ba9d2ea2c83bf4917f6c7795768
codgician <15964984+codgician@users.noreply.github.com>
2023-07-23 19:51:18 +0800

    init config for first host

--- flake.nix
+++ flake.nix
@@ -3,16 +3,25 @@
 
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
-      home-manager.url = "github:nix-community/home-manager";
+      home-manager.url = "github:nix-community/home-manager/release-23.05";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
-      
-      # nix will normally use the nixpkgs defined in home-managers inputs, we only want one copy of nixpkgs though
+     
+      impermanence.url = "github:nix-community/impermanence";
+      nixos-hardware.url = "github:NixOS/nixos-hardware/master";     
+
       darwin.url = "github:lnl7/nix-darwin";
       darwin.inputs.nixpkgs.follows = "nixpkgs"; 
   };
   
   # add the inputs declared above to the argument attribute set
-  outputs = { self, nixpkgs, home-manager, darwin }: {
+  outputs = inputs @ {
+    self, 
+    nixpkgs, 
+    home-manager,
+    impermanence,
+    darwin,
+    ...
+  }: {
     # we want `nix-darwin` and not gnu hello, so the packages stuff can go
     
     darwinConfigurations."Shijia-Mac" = darwin.lib.darwinSystem {
--- hosts/Shijia-Mac/default.nix
+++ hosts/Shijia-Mac/default.nix
@@ -1,18 +1,8 @@
 { pkgs, ... }: {
-  # Make sure the nix daemon always runs
-  services.nix-daemon.enable = true;
- 
-  # Installs a version of nix, that dosen't need "experimental-features = nix-command flakes" in /etc/nix/nix.conf
-  services.nix-daemon.package = pkgs.nixFlakes;
-  
   # if you use zsh (the default on new macOS installations),
   # you'll need to enable this so nix-darwin creates a zshrc sourcing needed environment changes
   programs.zsh = {
     enable = true;
-    ohMyZsh = {
-        enable = true;
-        theme = "half-life";
-    };
   };
 
   # Homebrew casks
@@ -29,7 +19,7 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     users.codgi = {
-      stateVersion = "23.05";
+      home.stateVersion = "23.05";
     };
   };
 }
