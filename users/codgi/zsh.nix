eabf2ccf493ec46b0d6aef7762eaa0f34d78de3d
codgician <15964984+codgician@users.noreply.github.com>
2023-09-14 20:03:09 +0800

    remove ssh-agent zsh plugin

--- users/codgi/zsh.nix
+++ users/codgi/zsh.nix
@@ -6,21 +6,14 @@
       enable = true;
       plugins = [
         "git"
-        "ssh-agent"
       ] ++ lib.optionals pkgs.stdenvNoCC.isDarwin [
         "macos"
       ];
       theme = "half-life";
     };
 
-    initExtra = ''
-      zstyle :omz:plugins:ssh-agent quiet yes
-    '' + lib.optionalString pkgs.stdenvNoCC.isDarwin (
-      ''
-        zstyle :omz:plugins:ssh-agent ssh-add-args --apple-load-keychain
-      '' + lib.optionalString osConfig.homebrew.enable ''
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-      ''
-    );
+    initExtra = lib.optionalString osConfig.homebrew.enable ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
   };
 }
