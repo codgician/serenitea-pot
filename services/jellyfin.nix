54beac5f3afc4f23aa8792e8f2c51d9012e63018
codgician <15964984+codgician@users.noreply.github.com>
2023-12-01 22:41:16 +0800

    misc: cleanup for jellyfin

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -37,24 +37,6 @@ in
       "sz6.codgician.me"
     ];
     group = config.services.nginx.user;
-
-    # Create .pfx for Jellyfin and load new certificate
-    postRun = ''
-      cat full.pem | ${pkgs.openssl}/bin/openssl pkcs12 -export -passout pass: -out cert.pfx
-      chmod --reference=full.pem cert.pfx
-      chown --reference=full.pem cert.pfx
-      systemctl restart jellyfin
-    '';
-  };
-
-  # Load certificate for Jellyfin
-  systemd.services.jellyfin = {
-    requires = [ "acme-finished-${domain}.target" ];
-    serviceConfig.LoadCredential =
-      let
-        certDir = config.security.acme.certs."${domain}".directory;
-      in
-      [ "cert.pfx:${certDir}/cert.pfx" ];
   };
 
   # Persist data
