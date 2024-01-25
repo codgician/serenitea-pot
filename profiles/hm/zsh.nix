63af797be4b19580c840e01fd5ac9366c72b087a
codgician <15964984+codgician@users.noreply.github.com>
2024-01-25 09:11:58 +0800

    zsh profile: only import brew shellenv for aarch64

--- profiles/hm/zsh.nix
+++ profiles/hm/zsh.nix
@@ -16,7 +16,9 @@
       zstyle :omz:plugins:ssh-agent quiet yes
     '' + lib.optionalString pkgs.stdenvNoCC.isDarwin (
       lib.optionalString osConfig.homebrew.enable ''
-        eval "$(/opt/homebrew/bin/brew shellenv)"
+        if [ -f /opt/homebrew/bin/brew ]; then 
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+        fi
       ''
     );
   };
