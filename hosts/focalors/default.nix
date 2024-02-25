47188fd23b890360c2447e411062a81ba80626bb
codgician <15964984+codgician@users.noreply.github.com>
2024-02-26 01:58:48 +0800

    fcitx5: use wayland frontend

--- hosts/focalors/default.nix
+++ hosts/focalors/default.nix
@@ -66,7 +66,10 @@
     };
     inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons ];
+      fcitx5 = {
+        addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons ];
+        waylandFrontend = true;
+      };
     };
   };
 
