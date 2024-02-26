e922ffb6112966a8871dd5b1deddee010ce92cbf
codgician <15964984+codgician@users.noreply.github.com>
2024-02-26 22:38:09 +0800

    focalors: enable parallels share auto mount

--- hosts/focalors/hardware.nix
+++ hosts/focalors/hardware.nix
@@ -3,7 +3,7 @@
 
   boot.supportedFilesystems = [ "bcachefs" ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "sr_mod" "prl_fs" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ ];
@@ -15,6 +15,10 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
-  hardware.parallels.enable = true;
+
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "prl-tools" ];
+  hardware.parallels = {
+    enable = true;
+    autoMountShares = true;
+  };
 }
