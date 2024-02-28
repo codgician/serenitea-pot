f2ca174fa42e494ae806754d3c68da9b7ea9cfd9
codgician <15964984+codgician@users.noreply.github.com>
2024-02-28 12:17:44 +0800

    agenix: create darwin-specific module

--- modules/darwin/system/agenix.nix
+++ modules/darwin/system/agenix.nix
@@ -1,2 +1,15 @@
-# The NixOS agenix module is compatible with darwin
-import ../../nixos/system/agenix.nix
+{ config, lib, pkgs, inputs, ... }:
+let
+  cfg = config.codgician.system.agenix;
+  impermanenceCfg = config.codgician.system.impermanence;
+in
+{
+  options.codgician.system.agenix = {
+    enable = lib.mkEnableOption "Enable agenix for secrets management.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Install agenix CLI
+    environment.systemPackages = [ inputs.agenix.packages.${pkgs.system}.default ];
+  };
+}
