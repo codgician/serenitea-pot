d5629310989bb2c37b5bd5c04e911458dafb4e6f
codgician <15964984+codgician@users.noreply.github.com>
2024-06-22 20:37:31 +0800

    wireguard: update profiles

--- modules/nixos/services/wireguard/peers/lumine.nix
+++ modules/nixos/services/wireguard/peers/lumine.nix
@@ -2,7 +2,7 @@
   name = "lumine";
   endpoint = "lumine.codgician.me:51820";
   listenPort = 51820;
-  ips = [ "192.168.254.64/23" "fd00:c0d9:1cff::64/48" ];
+  ips = [ "192.168.254.64/32" "fd00:c0d9:1cff::64/128" ];
   allowedIps = [ "192.168.64.0/24" "fd00:c0d9:1c64::/48" ];
   privateKeyFile = config.age.secrets.wgLuminePrivateKey.path;
   presharedKeyFile = config.age.secrets.wgPresharedKey.path;
