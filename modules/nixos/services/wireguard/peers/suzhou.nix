2030e62d7a2c24b8e852b7a47d61999d07f8c7d3
codgician <15964984+codgician@users.noreply.github.com>
2024-06-22 20:43:11 +0800

    wireguard: update profiles

--- modules/nixos/services/wireguard/peers/suzhou.nix
+++ modules/nixos/services/wireguard/peers/suzhou.nix
@@ -2,8 +2,8 @@
   name = "suzhou";
   endpoint = "sz.codgician.me:51820";
   listenPort = 51820;
-  ips = [ "192.168.254.1/32" "fd00:c0d9:1cff::1/128" ];
-  allowedIPs = [ "192.168.0.0/24" "fd00:c0d9:1c00::/48" ];
+  ips = [ "192.168.254.1/23" "fd00:c0d9:1cff::1/48" ];
+  allowedIPs = [ "192.168.254.1/32" "fd00:c0d9:1cff::1/128" "192.168.0.0/24" "fd00:c0d9:1c00::/48" ];
   privateKeyFile = config.age.secrets.wgSuzhouPrivateKey.path;
   presharedKeyFile = config.age.secrets.wgPresharedKey.path;
   publicKey = "1QYPFw1YL0Px4+41YvBLX4qgXH4KTz9JOqzNbMnlgR8=";
