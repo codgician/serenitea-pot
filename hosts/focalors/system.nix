c0758dc5b57559f884fd2513f8fa5d61d3a792c5
codgician <15964984+codgician@users.noreply.github.com>
2024-07-06 01:46:03 +0800

    focalors: add virt-manager

--- hosts/focalors/system.nix
+++ hosts/focalors/system.nix
@@ -135,12 +135,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   programs.kdeconnect.enable = true;
@@ -160,6 +154,7 @@
     htop
     firefox
     vscode
+    virt-manager
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
