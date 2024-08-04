6032be7db21b0626dc9a15ddda98edbbd528b279
codgician <15964984+codgician@users.noreply.github.com>
2024-08-04 23:08:10 +0800

    lib: refine secrets.nix

--- lib/secrets.nix
+++ lib/secrets.nix
@@ -1,25 +1,29 @@
 { lib, ... }: rec {
+  # Path to secrets dirctory
   secretsDir = ../secrets;
 
+  # Extract secret name from secret path
   getAgeSecretNameFromPath = path: lib.removeSuffix ".age" (builtins.baseNameOf path);
 
+  # Convert secret name to secret path
   getAgeSecretPathFromName = name: secretsDir + "/${name}.age";
 
+  # Make assertion rules for specified age file paths
   mkAgenixAssertions = builtins.map (file: {
     assertion = lib.pathExists file;
     message = "Credential '${file}' must exist.";
   });
 
+  # Make agenix config for specified owner and age file paths
   mkAgenixConfigs = owner: files: {
-    age.secrets = lib.codgician.concatAttrs (builtins.map
-      (file: {
-        "${getAgeSecretNameFromPath file}" = {
-          inherit file owner;
-          mode = "600";
-        };
-      })
-      files);
-
+    age.secrets = builtins.listToAttrs (builtins.map (file: {
+      name = getAgeSecretNameFromPath file;
+      value = {
+        inherit file owner;
+        mode = "600";
+      };
+    }) files);
+    
     assertions = mkAgenixAssertions files;
   };
 }
