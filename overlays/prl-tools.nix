bd05ed6ced8b6cd32fbc3fab8b553bcb9e39a03d
codgician <15964984+codgician@users.noreply.github.com>
2024-08-14 21:11:31 +0800

    overlays: always use prl-tools from unstable

---
+++ overlays/prl-tools.nix
@@ -0,0 +1,18 @@
+# Always use prl-tools from unstable
+
+{ inputs }:
+
+self: super:
+let
+  unstablePkgs = import inputs.nixpkgs-nixos-unstable {
+    inherit (super) system;
+    config.allowUnfree = true;
+  };
+in
+{
+  linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
+    prl-tools = unstablePkgs.linuxPackages.prl-tools.override {
+      inherit (lpsuper) kernel;
+    };
+  });
+}
