2fb2e897552fd71e220882d7df7bd2ad6abc5cdb
codgician <15964984+codgician@users.noreply.github.com>
2024-09-04 11:14:07 +0800

    yoimiya: update microcode

--- hosts/yoimiya/hardware.nix
+++ hosts/yoimiya/hardware.nix
@@ -79,7 +79,7 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   powerManagement = {
     cpuFreqGovernor = "powersave";
     powerUpCommands = ''
