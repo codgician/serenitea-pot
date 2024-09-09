0f5b7bd2b88f1c304eda7d08e44f8fbd79db9fe2
codgician <15964984+codgician@users.noreply.github.com>
2024-09-09 20:43:51 +0800

    nix: add my cachix

--- modules/generic/nix/default.nix
+++ modules/generic/nix/default.nix
@@ -26,11 +26,13 @@ in
             "https://nix-community.cachix.org"
             "https://cache.lix.systems"
             "https://cache.saumon.network/proxmox-nixos"
+            "https://codgician.cachix.org"
           ];
           trusted-public-keys = [
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
             "proxmox-nixos:nveXDuVVhFDRFx8Dn19f1WDEaNRJjPrF2CPD2D+m1ys="
+            "codgician.cachix.org-1:v4RtwkbJZJwfDxH5hac1lHehIX6JoSL726vk1ZctN8Y="
           ];
         }
 
