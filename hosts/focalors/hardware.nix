c129298bfe01bc9c1b1dfeab7ef2c22bf0f25865
codgician <15964984+codgician@users.noreply.github.com>
2024-09-12 10:15:47 +0800

    prl-tools: upgrade to v20

--- hosts/focalors/hardware.nix
+++ hosts/focalors/hardware.nix
@@ -13,4 +13,29 @@
     enable = true;
     autoMountShares = true;
   };
+
+  nix.settings = {
+    extra-platforms = [ "x86_64-linux" ];
+    extra-sandbox-paths = [
+      "/run/binfmt"
+      "/media/psf/RosettaLinux"
+    ];
+  };
+
+  # prlbinfmtconfig.sh would only register binfmt when systemd-binfmt.service is enabled.
+  # Following lines are added to ensure the service exists and is enabled when prlstoolsd.service runs
+  boot.binfmt.registrations.RosettaLinux = {
+    interpreter = "/media/psf/RosettaLinux/rosetta";
+
+    # The required flags for binfmt are documented by Apple:
+    # https://developer.apple.com/documentation/virtualization/running_intel_binaries_in_linux_vms_with_rosetta
+    magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+    mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+    fixBinary = true;
+    matchCredentials = true;
+    preserveArgvZero = false;
+
+    # Remove the shell wrapper and call the runtime directly
+    wrapInterpreterInShell = false;
+  };
 }
--- overlays/prl-tools.nix
+++ overlays/prl-tools.nix
@@ -11,8 +11,15 @@ let
 in
 {
   linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
-    prl-tools = unstablePkgs.linuxPackages.prl-tools.override {
+    prl-tools = (unstablePkgs.linuxPackages.prl-tools.override {
       inherit (lpsuper) kernel;
-    };
+    }).overrideAttrs (oldAttrs: rec {
+      version = "20.0.0-55653";
+      src = super.fetchurl {
+        url = "https://download.parallels.com/desktop/v${super.lib.versions.major version}/${version}/ParallelsDesktop-${version}.dmg";
+        hash = "sha256-ohGhaLVzXuR/mQ6ToeGbTixKy01F14JSgTs128vGZXM=";
+      };
+      buildInputs = oldAttrs.buildInputs ++ [ super.fuse ];
+    });
   });
 }
