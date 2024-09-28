84d7e5d2b56e0bb86a8f907a91b0e7e2737cb35f
codgician <15964984+codgician@users.noreply.github.com>
2024-09-28 13:22:51 +0800

    vscode: use unstable version

--- overlays/vscode-extensions.nix
+++
@@ -1,5 +0,0 @@
-{ inputs }:
-
-self: super: with inputs.nix-vscode-extensions.extensions.${super.system}; {
-  inherit vscode-marketplace vscode-marketplace-release open-vsx open-vsx-release;
-}
---
+++ overlays/vscode.nix
@@ -0,0 +1,14 @@
+{ inputs }:
+
+self: super:
+let
+  unstablePkgs = import inputs.nixpkgs-nixos-unstable {
+    inherit (super) system;
+    config.allowUnfree = true;
+  };
+in
+with inputs.nix-vscode-extensions.extensions.${super.system};
+{
+  inherit (unstablePkgs) vscode vscode-fhs vscode-with-extensions vscodium vscodium-fhs;
+  inherit vscode-marketplace vscode-marketplace-release open-vsx open-vsx-release;
+}
