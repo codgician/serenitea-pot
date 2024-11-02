52ff28b35acd486fe759b04333de8333e2d891e5
codgician <15964984+codgician@users.noreply.github.com>
2024-11-02 15:56:41 +0800

    cloudflare: add record akasha

--- terraform/cloudflare/zones/codgician-me/records/reverse-proxies.nix
+++ terraform/cloudflare/zones/codgician-me/records/reverse-proxies.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 let
   zone_id = config.resource.cloudflare_zone.codgician-me "id";
-  cnames = [ "amt" "books" "bubbles" "fin" "git" "hass" "matrix" "pve" "saw" ];
+  cnames = [ "akasha" "amt" "books" "bubbles" "fin" "git" "hass" "matrix" "pve" "saw" ];
 in
 {
   resource.cloudflare_record = builtins.listToAttrs (builtins.map
