0e1611ba87592102659bbf710c314513efe53e9e
codgician <15964984+codgician@users.noreply.github.com>
2024-11-18 22:08:21 +0800

    paimon: migrate state version to 24.11

--- hosts/paimon/system.nix
+++ hosts/paimon/system.nix
@@ -121,7 +121,7 @@
       zsh.enable = true;
     };
 
-    home.stateVersion = "24.05";
+    home.stateVersion = "24.11";
     home.packages = with pkgs; [ httplz screen ];
   };
 
@@ -137,5 +137,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
