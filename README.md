f438839f6e409fc2760a3991552d8d58a04ddd28
codgician <15964984+codgician@users.noreply.github.com>
2024-12-08 22:01:04 +0800

    ci: add secrets expiry check

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 [![Built with Nix](https://img.shields.io/static/v1?logo=nixos&logoColor=white&label=&message=Built%20with%20Nix&color=41439a)](https://builtwithnix.org)
 [![build](https://github.com/codgician/serenitea-pot/actions/workflows/build.yml/badge.svg)](https://github.com/codgician/serenitea-pot/actions/workflows/build.yml)
 [![evergreen](https://github.com/codgician/serenitea-pot/actions/workflows/evergreen.yml/badge.svg)](https://github.com/codgician/serenitea-pot/actions/workflows/evergreen.yml)
+[![expiry](https://github.com/codgician/serenitea-pot/actions/workflows/expiry.yml/badge.svg)](https://github.com/codgician/serenitea-pot/actions/workflows/expiry.yml)
 ![Man hours](https://manhours.aiursoft.cn/r/github.com/codgician/serenitea-pot.svg)  
 ![logo](https://github.com/codgician/serenitea-pot/assets/15964984/17d0e39c-9bee-4dd1-9aed-bb8d21f23daf)
 
---
+++ apps/chkexp/default.nix
@@ -0,0 +1,32 @@
+{ lib, inputs, ... }:
+
+let
+  thresholdInDays = 30;
+  nearestExpiryDate = lib.pipe (import (lib.codgician.secretsDir + "/secrets.nix")) [
+    builtins.attrValues
+    (builtins.filter (x: x?expiryDates))
+    (builtins.concatMap (x: x.expiryDates))
+    (builtins.foldl' lib.min "9999-12-31")
+  ];
+in
+# Check expiry for secrets
+lib.codgician.forAllSystems (pkgs: inputs.flake-utils.lib.mkApp {
+  drv = pkgs.writeShellApplication {
+    name = builtins.baseNameOf ./.;
+    runtimeInputs = with pkgs; [ coreutils ];
+    text = ''
+      current_time=$(date +%s)
+      nearest_expiry_time=$(date -d "${nearestExpiryDate} 00:00:00" +%s)
+      expiry_threshold=${builtins.toString (thresholdInDays * 24 * 60 * 60)}
+      
+      echo "Nearest expiry date: ${nearestExpiryDate}"
+      if (( nearest_expiry_time >= (current_time + expiry_threshold) )); then
+        echo "All secrets are far from expiry."
+        exit 0
+      else
+        echo "At least one secret is near expiry (within ${builtins.toString thresholdInDays} days)."
+        exit 1
+      fi
+    '';
+  };
+})
