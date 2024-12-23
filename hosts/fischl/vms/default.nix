b750f993902b7c91e9eaa81e25ae7f43bb3789c7
codgician <15964984+codgician@users.noreply.github.com>
2024-12-23 21:02:43 +0800

    fischl: isolate pinned cpus

--- hosts/fischl/vms/default.nix
+++ hosts/fischl/vms/default.nix
@@ -1,10 +1,36 @@
-{ ... }: {
+{ pkgs, ... }: {
   # libvirtd 
   virtualisation.libvirtd = {
     qemu.swtpm.enable = true;
     onBoot = "start";
     onShutdown = "shutdown";
     allowedBridges = [ "virbr0" ];
+    hooks.qemu = {
+      "10-isolate-cpu" = pkgs.writeShellApplication {
+        name = "qemu-hook";
+        runtimeInputs = with pkgs; [ systemd ];
+        text = ''
+          vm=$1
+          command=$2
+          if [ "$vm" != "openwrt" ]; then
+            echo "Skip running hook script for vm $vm"
+            exit 0
+          fi
+
+          if [ "$command" = "started" ]; then
+            echo "Limiting host CPU cores to 0,1,6,7"
+            systemctl set-property --runtime -- system.slice AllowedCPUs=0,1,6,7
+            systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,6,7
+            systemctl set-property --runtime -- init.scope AllowedCPUs=0,1,6,7
+          elif [ "$command" = "release" ]; then
+            echo "Releasing isolated CPU cores"
+            systemctl set-property --runtime -- system.slice AllowedCPUs=0-7
+            systemctl set-property --runtime -- user.slice AllowedCPUs=0-7
+            systemctl set-property --runtime -- init.scope AllowedCPUs=0-7
+          fi
+        '';
+      };
+    };
   };
 
   # Virtual machines
