584d171bd3fa2420414dd4385f3e641c2fe366b4
codgician <15964984+codgician@users.noreply.github.com>
2024-12-30 16:15:02 +0800

    prl-tools: use 20.1.3-55743

--- overlays/prl-tools.nix
+++ overlays/prl-tools.nix
@@ -1,18 +1,15 @@
-# Always use prl-tools from unstable
+# Bump to latest prl-tools
 
-{ inputs, ... }:
+{ lib, ... }:
 
-self: super:
-let
-  unstablePkgs = import inputs.nixpkgs-nixos-unstable {
-    inherit (super) system;
-    config.allowUnfree = true;
-  };
-in
-{
+self: super:{
   linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
-    prl-tools = (unstablePkgs.linuxPackages.prl-tools.override {
-      inherit (lpsuper) kernel;
-    });
+    prl-tools = lpsuper.prl-tools.overrideAttrs rec {
+      version = "20.1.3-55743";
+      src = super.fetchurl {
+        url = "https://download.parallels.com/desktop/v${lib.versions.major version}/${version}/ParallelsDesktop-${version}.dmg";
+        hash = "sha256-5lbTTQucop/jnsVudoqTO9bESR5tdn8NFu9Nm2WphU4=";
+      };
+    };
   });
 }
