a30529c6edc9c06a070bf4b68b03ea128d753d46
codgician <15964984+codgician@users.noreply.github.com>
2025-01-05 13:47:11 +0800

    overlays: revert prl-tools because bumped upstream

--- overlays/prl-tools.nix
+++ overlays/prl-tools.nix
@@ -1,15 +1,18 @@
-# Bump to latest prl-tools
+# Always use prl-tools from unstable
 
-{ lib, ... }:
+{ inputs, ... }:
 
-self: super: {
+self: super:
+let
+  unstablePkgs = import inputs.nixpkgs-nixos-unstable {
+    inherit (super) system;
+    config.allowUnfree = true;
+  };
+in
+{
   linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
-    prl-tools = lpsuper.prl-tools.overrideAttrs rec {
-      version = "20.1.3-55743";
-      src = super.fetchurl {
-        url = "https://download.parallels.com/desktop/v${lib.versions.major version}/${version}/ParallelsDesktop-${version}.dmg";
-        hash = "sha256-5lbTTQucop/jnsVudoqTO9bESR5tdn8NFu9Nm2WphU4=";
-      };
-    };
+    prl-tools = (unstablePkgs.linuxPackages.prl-tools.override {
+      inherit (lpsuper) kernel;
+    });
   });
 }
