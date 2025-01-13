8967390f268f74b4a9dbd4a00266053bf0d07b61
codgician <15964984+codgician@users.noreply.github.com>
2025-01-13 18:58:36 +0800

    flake: switch stable to 24.11-small

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11-small";
     nixpkgs-nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
