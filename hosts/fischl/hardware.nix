a4bceeaed5c4d84d9e3a84e0513000ea8d401699
codgician <15964984+codgician@users.noreply.github.com>
2025-01-20 00:18:58 +0800

    fischl: improve vfio binding for eth

--- hosts/fischl/hardware.nix
+++ hosts/fischl/hardware.nix
@@ -48,6 +48,15 @@
     "/boot-1".neededForBoot = true;
   };
 
+  # Bind first 3 ethernet cards to vfio for passthrough
+  boot.initrd.preDeviceCommands = ''
+    devs="0000:01:00.0 0000:02:00.0 0000:03:00.0"
+    for dev in $devs; do 
+      echo "vfio-pci" > /sys/bus/pci/devices/$dev/driver_override 
+    done
+    modprobe -i vfio-pci
+  '';
+
   # Sync content to backup ESP partition on activation
   system.activationScripts.rsync-esp.text = ''
     if ! ${pkgs.util-linux}/bin/mountpoint -q /boot-0; then
