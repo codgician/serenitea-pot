97aca03724c05a0a996a28f9911487e6b8c3af50
codgician <15964984+codgician@users.noreply.github.com>
2025-03-17 00:55:21 +0800

    shells/repl: exit bash when nix repl quits

--- shells/repl/default.nix
+++ shells/repl/default.nix
@@ -4,5 +4,6 @@ pkgs.mkShell {
   buildInputs = with pkgs; [ git ];
   shellHook = ''
     nix repl --expr "builtins.getFlake (builtins.toString $(git rev-parse --show-toplevel))"
+    exit $?
   '';
 }
