7f0776ec896fd67c099d98fe299f8456981309cd
codgician <15964984+codgician@users.noreply.github.com>
2025-05-12 21:06:55 +0800

    overlays: improve kernel module overiding

--- overlays/kernel-modules.nix
+++ overlays/kernel-modules.nix
@@ -8,27 +8,18 @@ let
     inherit (super) system;
     config.allowUnfree = true;
   };
-
-  linuxPackageNames = builtins.filter (name: builtins.match "linuxPackages.*" name != null) (
-    builtins.attrNames super
-  );
-
-  mkLinuxPackageOverride =
-    name:
-    super.${name}.extend (
-      lpself: lpsuper:
-      lib.genAttrs [
-        "mstflint_access"
-        "prl-tools"
-      ] (pkgName: unstablePkgs.linuxPackages.${pkgName}.override { inherit (lpsuper) kernel; })
-    );
 in
 {
   inherit (unstablePkgs) mstflint;
+  linuxKernel = super.linuxKernel // {
+    packagesFor =
+      kernel:
+      (super.linuxKernel.packagesFor kernel).extend (
+        lpself: lpsuper:
+        lib.genAttrs [
+          "mstflint_access"
+          "prl-tools"
+        ] (pkgName: unstablePkgs.linuxPackages.${pkgName}.override { inherit (lpsuper) kernel; })
+      );
+  };
 }
-// builtins.listToAttrs (
-  builtins.map (name: {
-    inherit name;
-    value = mkLinuxPackageOverride name;
-  }) linuxPackageNames
-)
