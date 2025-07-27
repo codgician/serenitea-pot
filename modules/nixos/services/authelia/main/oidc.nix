88bfb18a6cff050f016d6e6422842d12a71cb1ff
codgician <15964984+codgician@users.noreply.github.com>
2025-07-28 02:30:39 +0800

    authelia: add home-assistant oidc client

--- modules/nixos/services/authelia/main/oidc.nix
+++ modules/nixos/services/authelia/main/oidc.nix
@@ -97,5 +97,27 @@
       userinfo_signed_response_alg = "none";
       token_endpoint_auth_method = "client_secret_post";
     }
+    {
+      client_id = "home-assistant";
+      client_name = "Home Assistant";
+      client_secret = "$pbkdf2-sha512$310000$pT/gLgFD3bJ2/lWQBWROZA$c7N9VIP1OpKmmQsrnm8YmseHCA9BOaTcu0Wmlr890qyhTdsi1OC33yRHTgvS2zuAeH8B/lPuEnuot.vMpT2pAw";
+      public = false;
+      authorization_policy = "two_factor";
+      require_pkce = true;
+      pkce_challenge_method = "S256";
+      redirect_uris = [
+        "https://hass.codgician.me/auth/oidc/callback"
+      ];
+      scopes = [
+        "openid"
+        "profile"
+        "groups"
+      ];
+      response_types = [ "code" ];
+      grant_types = [ "authorization_code" ];
+      access_token_signed_response_alg = "none";
+      userinfo_signed_response_alg = "none";
+      token_endpoint_auth_method = "client_secret_post";
+    }
   ];
 }
