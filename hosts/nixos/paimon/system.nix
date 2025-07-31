d13979b7a4093c60e5b17776b8aac1f784ff01f7
codgician <15964984+codgician@users.noreply.github.com>
2025-08-01 00:38:48 +0800

    paimon: deploy qwen3 coder 30b

--- hosts/nixos/paimon/system.nix
+++ hosts/nixos/paimon/system.nix
@@ -73,6 +73,7 @@
         loadModels = [
           "hf.co/unsloth/Qwen3-30B-A3B-Instruct-2507-GGUF:Q4_K_M"
           "hf.co/unsloth/Qwen3-30B-A3B-Thinking-2507-GGUF:Q4_K_M"
+          "hf.co/unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M"
           "hf.co/unsloth/Qwen3-32B-GGUF:Q4_K_M"
           "huihui_ai/qwen3-abliterated:30b-a3b-q4_K_M"
           "mistral-small3.2:24b"
