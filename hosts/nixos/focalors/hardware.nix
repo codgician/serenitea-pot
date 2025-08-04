c036e64f3c21bad9028bfbaa35e310225608dd73
codgician <15964984+codgician@users.noreply.github.com>
2025-08-05 02:20:18 +0800

    focalors: disable RosettaLinux

--- hosts/nixos/focalors/hardware.nix
+++ hosts/nixos/focalors/hardware.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
   boot = {
     initrd = {
@@ -9,6 +9,7 @@
       kernelModules = [ ];
     };
 
+    supportedFilesystems = [ "vfat" "zfs "];
     kernelModules = [ ];
     kernelParams = [ "video=Virtual-1:3024x1890@120" ];
     extraModulePackages = [ ];
@@ -27,24 +28,24 @@
     extra-platforms = [ "x86_64-linux" ];
     extra-sandbox-paths = [
       "/run/binfmt"
-      "/media/psf/RosettaLinux"
+      # "/media/psf/RosettaLinux"
     ];
   };
 
   # prlbinfmtconfig.sh would only register binfmt when systemd-binfmt.service is enabled.
   # Following lines are added to ensure the service exists and is enabled when prlstoolsd.service runs
-  boot.binfmt.registrations.RosettaLinux = {
-    interpreter = "/media/psf/RosettaLinux/rosetta";
+  # boot.binfmt.registrations.RosettaLinux = {
+  #   interpreter = "/media/psf/RosettaLinux/rosetta";
 
-    # The required flags for binfmt are documented by Apple:
-    # https://developer.apple.com/documentation/virtualization/running_intel_binaries_in_linux_vms_with_rosetta
-    magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
-    mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
-    fixBinary = true;
-    matchCredentials = true;
-    preserveArgvZero = false;
+  #   # The required flags for binfmt are documented by Apple:
+  #   # https://developer.apple.com/documentation/virtualization/running_intel_binaries_in_linux_vms_with_rosetta
+  #   magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+  #   mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+  #   fixBinary = true;
+  #   matchCredentials = true;
+  #   preserveArgvZero = false;
 
-    # Remove the shell wrapper and call the runtime directly
-    wrapInterpreterInShell = false;
-  };
+  #   # Remove the shell wrapper and call the runtime directly
+  #   wrapInterpreterInShell = false;
+  # };
 }
