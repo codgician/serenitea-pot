ffb729acaa0deace1b3d282c83023b82b1e7c1b1
codgician <15964984+codgician@users.noreply.github.com>
2025-10-19 11:42:58 +0800

    paimon: add models

--- hosts/nixos/paimon/system.nix
+++ hosts/nixos/paimon/system.nix
@@ -80,6 +80,7 @@
         acceleration = "cuda";
         modelDir = "/xpool/llm/ollama/models";
         loadModels = [
+          "hf.co/unsloth/gpt-oss-20b-GGUF:F16"
           "hf.co/unsloth/Qwen3-30B-A3B-Instruct-2507-GGUF:Q4_K_M"
           "hf.co/unsloth/Qwen3-30B-A3B-Thinking-2507-GGUF:Q4_K_M"
           "hf.co/unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M"
--- modules/nixos/services/litellm/models.nix
+++ modules/nixos/services/litellm/models.nix
@@ -68,6 +68,7 @@ rec {
         };
       })
       [
+        "claude-haiku-4.5"
         "claude-sonnet-4"
         "claude-sonnet-4.5"
         "claude-opus-41"
