7cac2a47f67b2061beb216b7b58052a3621985bc
codgician <15964984+codgician@users.noreply.github.com>
2026-07-21 16:23:06 +0800

    plasma: enable NIXOS_OZONE_WL

--- modules/nixos/services/plasma/default.nix
+++ modules/nixos/services/plasma/default.nix
@@ -39,6 +39,8 @@ in
 
     # Enable dconf
     programs.dconf.enable = true;
+    # Nixpkgs Electron wrappers use this to enable native Wayland and its IME protocol.
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     # Auto unlock Kwallet
     security.pam.services = {
