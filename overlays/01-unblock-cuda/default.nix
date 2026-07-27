2f495e0cd2052b4bcf032a4ca58d4ac75faed1c0
codgician <15964984+codgician@users.noreply.github.com>
2026-07-28 01:17:24 +0800

    overlays: add cuda build workaround

---
+++ overlays/01-unblock-cuda/default.nix
@@ -0,0 +1,22 @@
+# Temporarily unblock cuda build as seen in https://github.com/NixOS/nixpkgs/issues/544701
+# TODO: remove after fix PR https://github.com/NixOS/nixpkgs/pull/545542 merged upstream
+
+{ lib, ... }:
+
+final: prev: {
+  cudaPackages = prev.cudaPackages.overrideScope (
+    cFinal: cPrev: {
+      setupCudaHook = cPrev.pkgs.makeSetupHook {
+        name = "setup-cuda-hook";
+
+        substitutions.setupCudaHook = placeholder "out";
+
+        # Required in addition to ccRoot as otherwise bin/gcc is looked up
+        # when building CMakeCUDACompilerId.cu
+        substitutions.ccFullPath = "${cPrev.backendStdenv.cc}/bin/${cPrev.backendStdenv.cc.targetPrefix}c++";
+
+        meta.license = lib.licenses.mit;
+      } ./setup-cuda-hook.sh;
+    }
+  );
+}
