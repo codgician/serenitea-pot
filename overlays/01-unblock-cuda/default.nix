7f7ad9623862cf31cc2add29aa2c71ade74fcd5e
codgician <15964984+codgician@users.noreply.github.com>
2026-07-28 09:46:09 +0800

    paimon: mitigate cuda build

--- overlays/01-unblock-cuda/default.nix
+++ overlays/01-unblock-cuda/default.nix
@@ -3,20 +3,28 @@
 
 { lib, ... }:
 
-final: prev: {
-  cudaPackages = prev.cudaPackages.overrideScope (
-    cFinal: cPrev: {
-      setupCudaHook = cPrev.pkgs.makeSetupHook {
-        name = "setup-cuda-hook";
+let
+  cudaFix = _final: prev: {
+    cudaPackages = prev.cudaPackages.overrideScope (
+      _cFinal: cPrev: {
+        setupCudaHook = cPrev.pkgs.makeSetupHook {
+          name = "setup-cuda-hook";
 
-        substitutions.setupCudaHook = placeholder "out";
+          substitutions.setupCudaHook = placeholder "out";
 
-        # Required in addition to ccRoot as otherwise bin/gcc is looked up
-        # when building CMakeCUDACompilerId.cu
-        substitutions.ccFullPath = "${cPrev.backendStdenv.cc}/bin/${cPrev.backendStdenv.cc.targetPrefix}c++";
+          # Required in addition to ccRoot as otherwise bin/gcc is looked up
+          # when building CMakeCUDACompilerId.cu
+          substitutions.ccFullPath = "${cPrev.backendStdenv.cc}/bin/${cPrev.backendStdenv.cc.targetPrefix}c++";
 
-        meta.license = lib.licenses.mit;
-      } ./setup-cuda-hook.sh;
-    }
-  );
+          meta.license = lib.licenses.mit;
+        } ./setup-cuda-hook.sh;
+      }
+    );
+  };
+in
+final: prev:
+(cudaFix final prev)
+// {
+  # `prev.unstable` is imported separately, so local overlays do not reach it.
+  unstable = prev.unstable.extend cudaFix;
 }
