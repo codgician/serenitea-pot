d8ce4c8e135951b5728b770f9a0525dd4b637df7
codgician <15964984+codgician@users.noreply.github.com>
2026-08-03 12:31:47 +0800

    terraform: mitigate attic upload 524

--- packages/terraform-config/cloudflare/zones/codgician-me/records/cache.nix
+++ packages/terraform-config/cloudflare/zones/codgician-me/records/cache.nix
@@ -10,18 +10,20 @@ in
     description = "CNAME target for the binary cache";
   };
 
-  resource.cloudflare_dns_record = builtins.listToAttrs (
-    builtins.map (name: {
-      name = "${name}-cname";
-      value = {
-        name = "${name}.${zone_name}";
-        proxied = true;
-        ttl = 1;
-        comment = "Binary Cache";
-        type = "CNAME";
-        content = "\${var.binary_cache_cname}";
-        inherit zone_id;
-      };
-    }) cnames
-  );
+  resource = {
+    cloudflare_dns_record = builtins.listToAttrs (
+      builtins.map (name: {
+        name = "${name}-cname";
+        value = {
+          name = "${name}.${zone_name}";
+          proxied = false;
+          ttl = 1;
+          comment = "Binary Cache";
+          type = "CNAME";
+          content = "\${var.binary_cache_cname}";
+          inherit zone_id;
+        };
+      }) cnames
+    );
+  };
 }
